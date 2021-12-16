90831ad59e06f9a753de0d70502186d3787d3def
Pranav Choudhary <2018184@iiitdmj.ac.in>
2021-12-16 16:57:31 +0530

    Toggle Image Added

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -3,17 +3,24 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ItemsController extends Controller {
+  @tracked color = this.model.colors[0].color;
 
-    @tracked color = this.model.colors[0].color;
+  @tracked isZoomed = true;
+
+  get productImage() {
+    return this.model.colors.find(({ color }) => color === this.color).image;
+  }
+
+  @action
+  changeColor(newColor) {
+    this.color = newColor;
+  }
+
+  @action
+  toggleZoom() {
+    this.isZoomed = !this.isZoomed;
+  }
 
 
-    get productImage() {
-        return this.model.colors.find(({ color }) => color === this.color).image;
-    }
-
-    @action
-    changeColor(newColor) {
-        this.color = newColor;
-    }
 
 }
---
+++ app/helpers/currency.js
@@ -0,0 +1,32 @@
+// import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
+
+// export default helper(function currency(params, hash = {}) {
+//   const number = params;
+//   let { sign = '$' } = hash;
+
+//   const dollars = Math.floor(number);
+//   let cents = Math.floor(number * 100 % 100);
+//   if (cents.toString().length === 1) {
+//     cents = '0' + cents;
+//   }
+
+
+//   return `${sign}${dollars}.${cents}`;
+// });
+
+export default class currency extends Helper {
+  compute(params, hash = {}) {
+    const [number] = params;
+    let { sign = '$' } = hash;
+
+    const dollars = Math.floor(number);
+    let cents = Math.floor(number * 100 % 100);
+    if (cents.toString().length === 1) {
+      cents = '0' + cents;
+    }
+
+
+    return `${sign}${dollars}.${cents}`;
+  }
+}
